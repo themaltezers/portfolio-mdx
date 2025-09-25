a1370a49730495aac8e9727d6473199653809da9
themaltezers <themaltezers@gmail.com>
2025-09-25 21:58:53 +0200

    modification lien zoooom

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -2,8 +2,6 @@ import { NextIntlClientProvider, hasLocale } from "next-intl";
 import { notFound } from "next/navigation";
 import { routing } from "@/i18n/routing";
 import Header from "@/components/Header";
-import ThemeToggle from "@/components/ThemeToggle";
-import LanguageSwitcher from "@/components/LanguageSwitcher";
 
 export default async function LocaleLayout({
     children,
@@ -19,7 +17,6 @@ export default async function LocaleLayout({
     return (
         <NextIntlClientProvider locale={locale} messages={messages}>
             <Header />
-
             <main style={{ padding: "2rem" }}>{children}</main>
         </NextIntlClientProvider>
     );
